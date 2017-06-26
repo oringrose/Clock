0bdcb8c4a4b3019ddf8c6b477f50ea6088fcf50b
oringrose <oliringrose@hotmail.com>
2017-06-26 15:57:33 +0100

    Updated html css and jscript files
    added daytime animation background with clouds blue sky and sun
    
    fixed the seconds hand on 0 going all the way round

--- clock.js
+++ clock.js
@@ -5,12 +5,14 @@ function setDate() {
 var sHand = document.getElementById("seconds");
 var mHand = document.getElementById("minutes");
 var hHand = document.getElementById("hours");
+var wholeHand = document.getElementsByClassName("hand")[0];
 
 var now = new Date();
 
 var seconds = now.getSeconds();
 var minutes = now.getMinutes();
 var hours   = now.getHours();
+
 
 
 var sDegree = ((seconds / 60) * 360) + 90;
@@ -23,10 +25,65 @@ mHand.style.transform = `rotate(${mDegree}deg)`;
 hHand.style.transform = `rotate(${hDegree}deg)`;
 
 console.log(seconds);
-console.log("minutes are " + minutes);
-console.log("hour is  " + hours);
+//console.log("minutes are " + minutes);
+//console.log("hour is  " + hours);
+
+
+
+if (seconds == 0) {
+wholeHand.classList.add("playing");
 
 }
+else {
+wholeHand.classList.remove("playing");
+}
+/*
+if (hours>=6 && hours<=22) {
+DAY TIME
+}
+
+else {
+NIGHT TIME
+}
+*/
+
+
+}
+
+function dayOrNight() {
+
+var backgroundCol = document.getElementsByTagName("body")[0];
+var sun = document.getElementsByClassName("svg-sun")[0];
+var clouds = document.getElementsByClassName("clouds-container")[0];
+
+var today = new Date();
+var todayHours = today.getHours();
+
+
+console.log(todayHours);
+
+if (todayHours >= 7 && todayHours <= 22) {
+
+sun.style.display = "absolute";
+backgroundCol.style.background = "#2980b9";
+clouds.style.display = "absolute";
+
+} else {
+
+sun.style.display = "none";
+backgroundCol.style.background = "black";
+clouds.style.display = "none";
+
+}
+
+}
+
+
+
+
+function dateToday() {
+
+
 var myDate = new Date();
 console.log(myDate);
 
@@ -37,8 +94,10 @@ var dateTime = currentDate.getDate() + "/"
              + (currentDate.getMonth()+1) + "/"
              + currentDate.getFullYear();
 
-console.log(dateTime);
+
 
 cDate.textContent = "The Date is " + dateTime;
-
+}
+dayOrNight();
+dateToday();
 setInterval(setDate, 1000);
